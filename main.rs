bc8b2d86c32c396659c98d661e98de85e26c1bb0
Gustavo dos Santos Leão <102435052+agatrix@users.noreply.github.com>
2025-06-13 09:39:31 -0300

    Add_ Função Crescente

---
+++ main.rs
@@ -0,0 +1,22 @@
+fn ordemCrescente(lista: &mut Vec<i64>) {
+    let n = lista.len();
+    for i in 1..n {
+        let mut j = i;
+        while j > 0 && lista[j - 1] > lista[j] {
+            lista.swap(j, j - 1);
+            j -= 1;
+        }
+    }
+}
+
+fn executar_estrategia(lista: &mut Vec<i64>, estrategia: fn(&mut Vec<i64>)) {
+    estrategia(lista);
+}
+
+fn main() {
+    let mut lista: Vec<i64> = vec![2,3,1,5,4];
+    executar_estrategia(&mut lista,ordemCrescente);
+ 
+    
+    
+}
