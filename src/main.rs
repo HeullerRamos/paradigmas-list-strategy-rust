c7900db966d60fd20c4c98b6fdab41e9aebca771
andref03 <andref.lopes@outlook.com>
2025-06-14 18:15:24 -0300

    atualizando a função de filtragem de pares

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-mod estrategias;
+fn filtrar_pares(vetor: &mut Vec<i64>) {
+    let mut pares = Vec::new();
+
+    for valor in vetor.iter() {
+        if valor % 2 == 0 && !pares.contains(valor) {
+            pares.push(*valor);
+        } 
+    }
+
+    *vetor = pares;
+}
 
 fn executar_estrategia(mut lista: Vec<i64>, estrategia: fn(&mut Vec<i64>)) {
     estrategia(&mut lista);
@@ -9,5 +19,5 @@ fn main() {
     let numeros = vec![5, 2, 8, 5, 2, 9, 1, 0, 4, 4];
     println!("Lista original: {:?}", numeros);
 
-    executar_estrategia(numeros, estrategias::filtrar_pares);
+    executar_estrategia(numeros, filtrar_pares);
 }
--- target/debug/deps/exercicio_09.d
+++ target/debug/deps/exercicio_09.d
@@ -1,6 +1,5 @@
-C:\Users\andre\CodigosParadigmas\exercicio_09\target\debug\deps\exercicio_09.exe: src\main.rs src\estrategias.rs
+C:\Users\andre\CodigosParadigmas\exercicio_09\target\debug\deps\exercicio_09.exe: src\main.rs
 
-C:\Users\andre\CodigosParadigmas\exercicio_09\target\debug\deps\exercicio_09.d: src\main.rs src\estrategias.rs
+C:\Users\andre\CodigosParadigmas\exercicio_09\target\debug\deps\exercicio_09.d: src\main.rs
 
 src\main.rs:
-src\estrategias.rs:
