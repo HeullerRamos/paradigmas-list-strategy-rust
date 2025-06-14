85b8e7cbd237d7fd9dff7d159fa92802d8969188
andref03 <andref.lopes@outlook.com>
2025-06-14 19:00:38 -0300

    Implementa função filtrar_pares e adiciona no main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn filtrar_pares(vetor: &mut Vec<i64>) {
     let mut pares = Vec::new();
 
     for valor in vetor.iter() {
-        if valor % 2 == 0 && !pares.contains(valor) {
+        if valor % 2 == 0 {
             pares.push(*valor);
         } 
     }
