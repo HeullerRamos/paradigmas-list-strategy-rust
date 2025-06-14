2e2417ad53dd7f5379147d2d46608898dd197bce
root <root@DESKTOP-A0FL8MG.localdomain>
2025-06-14 19:01:22 -0300

    add funçaõ removeDuplicatas

--- main.rs
+++ main.rs
@@ -8,7 +8,14 @@ fn ordemCrescente(lista: &mut Vec<i64>) {
         }
     }
 }
+use std::collections::HashSet;//para remover duplicatas
+fn removeDuplicatas(lista: &mut Vec<i64>) {
+    // 1. Cria um HashSet para rastrear os elementos que já vimos.
+    let mut vistos = HashSet::new();
 
+    // 2. Usa o método `retain` para manter apenas os elementos únicos.
+    lista.retain(|elemento| vistos.insert(*elemento));
+}
 fn imprimirLista(lista: &Vec<i64>){
     println!("{:?}", lista);
 }
@@ -18,8 +25,9 @@ fn executar_estrategia(lista: &mut Vec<i64>, estrategia: fn(&mut Vec<i64>)) {
 }
 
 fn main() {
-    let mut lista: Vec<i64> = vec![2,3,1,5,4];
+    let mut lista: Vec<i64> = vec![2,3,1,5,4,1,3,5,3,1,34,4,12,3];
     executar_estrategia(&mut lista,ordemCrescente);
+    executar_estrategia(&mut lista,removeDuplicatas);
     imprimirLista(&lista);
     
     
